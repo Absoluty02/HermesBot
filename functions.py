3ea61b1766d6e5bad89d1cb4883f526cdd64318d
Jacopo Garofalo <jacopogarofalo02@gmail.com>
2023-11-04 13:35:25 +0100

    added functionalities + refined search news functionality

--- functions.py
+++ functions.py
@@ -1,8 +1,11 @@
+import re
+
 import telebot
 import configparser
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 from newsapi import NewsApiClient
+import requests
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -15,6 +18,55 @@ newsapi = NewsApiClient(api_key=config.get('private', 'newsApi_key'))
 uri = "mongodb+srv://BotUser:RtMw8xAdwyFjU924@clusterbot.rjqkcg5.mongodb.net/?retryWrites=true&w=majority"
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
+db = client[config.get('private', 'db_name')]
 
 
+def check_url(url):
+    regex = "^((http|https)://)[-a-zA-Z0-9@:%._\\+~#?&//=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%._\\+~#?&//=]*)$"
+    r = re.compile(regex)
 
+    if re.search(r, url):
+        request = requests.get(url)
+        if request.status_code == 200:
+            return True
+        else:
+            return False
+    else:
+        return False
+
+
+def save_news_url(message, custom_name_message):
+    url_site = message.text
+    user_chat_id = message.chat.id
+
+    existing_news = db.get_collection('news').find_one({'url': url_site})
+    if not existing_news:
+        print("notizia non trovata")
+        db.get_collection('news').insert_one({
+            'url': url_site,
+            'interested_users': [{
+                "user_chat_id": user_chat_id,
+                "name": custom_name_message.text}
+            ]
+        })
+        return True
+    else:
+        coso = db.get_collection('news').find_one({
+            'url': url_site,
+            'interested_users': {
+                "user_chat_id": user_chat_id
+            }
+        })
+        if not coso:
+            return False
+        else:
+            db.get_collection('news').update_one({
+                'url': url_site},
+                {"$addToSet":
+                    {"interested_users":
+                        {
+                            "user_chat_id": user_chat_id,
+                            "name": custom_name_message.text
+                        }}}
+            )
+            return True
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import re
+
 import telebot
 from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup
 import configparser
+from functions import check_url, save_news_url
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 from newsapi import NewsApiClient
@@ -19,41 +22,146 @@ client = MongoClient(uri, server_api=ServerApi('1'))
 def search_markup_inline():
     markup = InlineKeyboardMarkup()
     markup.row_width = 2
-    markup.add(InlineKeyboardButton("Categoria", callback_data="cat", ),
-               InlineKeyboardButton("Parola chiave", callback_data="tag")
-               )
+    markup.add(
+        InlineKeyboardButton("Categoria", callback_data="cat"),
+        InlineKeyboardButton("Parola chiave", callback_data="tag")
+    )
     return markup
 
 
-@bot.message_handler(commands=["Ciao"])
-def greet(message):
-    print(message.chat.first_name)
-    bot.reply_to(message, "Ciao a te! Bentornato su Telegram.")
+def continue_category_search_markup_inline(current_page, category):
+    markup = InlineKeyboardMarkup()
+    markup.row_width = 2
+    markup.add(
+        InlineKeyboardButton("Si", callback_data="searching_yes_cat_" + category + "_"
+                                                 + str(current_page)),
+        InlineKeyboardButton("No", callback_data="searching_no"),
+        InlineKeyboardButton("Cambia categoria", callback_data="searching_change_cat"),
+        InlineKeyboardButton("Cambia ricerca", callback_data="searching_change_search")
+    )
+    return markup
 
 
-@bot.message_handler(commands=["CiaoBot"])
-def greetWithoutResponse(message):
-    bot.send_message(message.chat.id, "Ciao a te! Bentornato su Telegram.")
+def continue_tag_search_markup_inline(current_page, tag):
+    markup = InlineKeyboardMarkup()
+    markup.row_width = 2
+    markup.add(
+        InlineKeyboardButton("Si", callback_data="searching_yes_tag_" + tag + "_" + str(current_page)),
+        InlineKeyboardButton("No", callback_data="searching_no"),
+        InlineKeyboardButton("Cambia parola chiave", callback_data="searching_change_tag"),
+        InlineKeyboardButton("Cambia ricerca", callback_data="searching_change_search")
+    )
+    return markup
+
+
+def preferences_markup(user):
+    markup = InlineKeyboardMarkup()
+    markup.row_width = 2
+    markup.add(
+        InlineKeyboardButton("Cambia numero", callback_data="news_req"),
+        InlineKeyboardButton("Disattiva localizzazione", callback_data="localization") if user['localization'] else
+        InlineKeyboardButton("Attiva localizzazione", callback_data="localization"),
+        InlineKeyboardButton("Aggiungi dominio", callback_data="add_domain"),
+        InlineKeyboardButton("Rimuovi dominio", callback_data="remove_domain")
+    )
+    return markup
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def bot_callback(call):
+    if call.data == "cat":
+        msg = bot.send_message(call.message.chat.id, "Dimmi la categoria di notizie a cui sei interessato")
+        bot.register_next_step_handler(msg, search_for_category, {
+            'current_page': 1,
+            'category': None
+        })
+    elif call.data == "tag":
+        msg = bot.send_message(call.message.chat.id, "Dimmi la fonte di notizie a cui sei interessato")
+        bot.register_next_step_handler(msg, search_for_tag, {
+            'current_page': 1,
+            'tag': None
+        })
+    else:
+        print("ciao")
+        r1 = r'yes'
+        r2 = r'no'
+        r3 = r'change'
+        if re.search(r1, call.data):
+            print("ciaociao")
+            data = call.data.split("_")
+            if data[1] == 'cat':
+                search_for_category(call.message, {
+                    'current_page': int(data[4]),
+                    'category': data[3]
+                })
+            if data[1] == 'tag':
+                print("ciaociaociao")
+                search_for_tag(call.message, {
+                    'current_page': int(data[4]),
+                    'tag': data[3]
+                })
+        if re.search(r2, call.data):
+            pass
+        if re.search(r3, call.data):
+            data = call.data.split("_")
+            if data[2] == "search":
+                search(call.message)
+            if data[2] == "cat":
+                msg = bot.send_message(call.message.chat.id, "Dimmi la categoria di notizie a cui sei interessato")
+                bot.register_next_step_handler(msg, search_for_category, {
+                    'current_page': 1,
+                    'category': None
+                })
+            if data[2] == "tag":
+                msg = bot.send_message(call.message.chat.id, "Dimmi la fonte di notizie a cui sei interessato")
+                bot.register_next_step_handler(msg, search_for_tag, {
+                    'current_page': 1,
+                    'tag': None
+                })
 
 
 @bot.message_handler(commands=['start'])
-def startConversation(message):
+def start_conversation(message):
     existing_user = db.get_collection('users').find_one({'chat_id': message.chat.id})
     if not existing_user:
         db.get_collection('users').insert_one({
             'chat_id': message.chat.id,
-            'username': message.chat.first_name
-        })
+            'username': message.chat.first_name,
+            'localization': False,
+            'news_for_request': 5,
+            'excluded_domains': [],
+            'news': []})
+
     bot.send_message(message.chat.id,
                      f"Benvenuto {message.chat.first_name}, io sono Hermes, utilizza i comandi per poter "
                      f"gestire le tue notizie preferite: ")
 
 
 @bot.message_handler(commands=['salvaNotizia'])
-def saveNews(message):
-    start = bot.send_message(message.chat.id, "Ottimo, mandami mi qui di seguito l'URL del sito dove hai "
+def save_news(message):
+    start = bot.send_message(message.chat.id, "Ottimo, mandami qui di seguito l'URL del sito dove hai "
                                               "notato la notizia che ti interessa")
-    bot.register_next_step_handler(start, saveSecondStep)
+    bot.register_next_step_handler(start, save_second_step)
+
+
+def save_second_step(message):
+    if not check_url(message.text):
+        bot.send_message(message.chat.id, "L'URL che hai fornito non è valido, riprova con un altro link")
+    else:
+        url_site = message
+        msg = bot.reply_to(message, "Perfetto, dimmi con che nome vorresti salvare questo link.")
+        bot.register_next_step_handler(msg, save_third_step, url_site)
+
+
+def save_third_step(message, url_site):
+    if type(message.text) is str:
+        if save_news_url(url_site, message):
+            bot.send_message(message.chat.id, "Link alla notizia aggiunto con successo!")
+        else:
+            bot.send_message(message.chat.id, "Sembra che avevi già salvato in precedenza questo link, riprova con un "
+                                              "altro")
+    else:
+        bot.send_message(message.chat.id, "Nome inserito non valido, riprova")
 
 
 @bot.message_handler(commands=['cercaNotizia'])
@@ -62,37 +170,78 @@ def search(message):
                      reply_markup=search_markup_inline())
 
 
-@bot.callback_query_handler(func=lambda call: True)
-def search_callback(call):
-    if call.data == "cat":
-        msg = bot.send_message(call.message.chat.id, "Dimmi la categoria di notizie a cui sei interessato")
-        bot.register_next_step_handler(msg, searchForCategory)
-    if call.data == "tag":
-        msg = bot.send_message(call.message.chat.id, "Dimmi la fonte di notizie a cui sei interessato")
-        bot.register_next_step_handler(msg, searchForTag)
+def search_for_category(message, details):
+    bot.send_message(message.chat.id, f"Ecco le notizie di pagina {details['current_page']}")
+    logged_user = db.get_collection('users').find_one({'chat_id': message.chat.id})
+
+    if details['category'] is None:
+        results = newsapi.get_top_headlines(category=message.text, page=details['current_page'],
+                                            page_size=logged_user['news_for_request'])
+        current_cat = message.text
+    else:
+        results = newsapi.get_top_headlines(category=details['category'], page=details['current_page'],
+                                            page_size=logged_user['news_for_request'])
+        current_cat = details['category']
+    send_news_messages(results["articles"], message.chat.id)
+    bot.send_message(message.chat.id, text="Desideri altre notizie?",
+                     reply_markup=continue_category_search_markup_inline(details['current_page'] + 1,
+                                                                         current_cat))
 
 
-def searchForCategory(message):
-    results = newsapi.get_top_headlines(category=message.text, page=1, page_size=5)
-    news = results["articles"]
+def send_news_messages(news, chat_id):
     for n in news:
-        source = n['source']
-        print(source)
-        strSource = str(source)
-        print(strSource)
-        currMsg = "*Titolo*: " + n['source']['name']
-        bot.send_message(message.chat.id, text=currMsg, parse_mode="MarkdownV2")
-
-def searchForTag(message):
-    result = newsapi.get_top_headlines(q=message.text, page=1, page_size=5)
-    news = result["articles"]
-    for n in news:
-        currMsg = "**Titolo**: " + n["source"]
-        bot.send_message(message.chat.id, text=currMsg, parse_mode="MarkdownV2")
+        print(n)
+        curr_msg = f"<b>Titolo</b>: {n['title']}\n"
+        if n['author'] is None:
+            curr_msg += "<b>Autore</b>: Non fornito\n"
+        else:
+            curr_msg += f"<b>Autore</b> : {n['author']}\n"
+        curr_msg += f"<b>Descrizione</b>: {n['description']}\n"
+        curr_msg += f"<b>Link alla notizia</b>: <a href='{n['url']}'>link personalizzato</a>"
+        bot.send_message(chat_id, text=curr_msg, parse_mode="HTML")
 
 
-def saveSecondStep(message):
-    bot.send_message(message.chat.id, "ciao")
+def search_for_tag(message, details):
+    bot.send_message(message.chat.id, f"Ecco le notizie di pagina {details['current_page']}")
+    logged_user = db.get_collection('users').find_one({'chat_id': message.chat.id})
+    if details['tag'] is None:
+        result = newsapi.get_everything(q=message.text, page=details['current_page'],
+                                        page_size=logged_user['news_for_request'], sort_by="relevancy")
+        current_tag = message.text
+    else:
+        result = newsapi.get_everything(q=details['tag'], page=details['current_page'],
+                                        page_size=logged_user['news_for_request'], sort_by="relevancy")
+        current_tag = details['tag']
+    send_news_messages(result["articles"], message.chat.id)
+    bot.send_message(message.chat.id, text="Desideri altre notizie?",
+                     reply_markup=continue_tag_search_markup_inline(details['current_page'] + 1, current_tag))
+
+
+@bot.message_handler(commands=['visualizzaNotizie'])
+def show_news(message):
+    logged_user = db.get_collection('users').find_one({'chat_id': message.chat.id})
+
+
+@bot.message_handler(commands=['gestisciPreferenze'])
+def manage_preferences(message):
+    logged_user = db.get_collection('users').find_one({'chat_id': message.chat.id})
+    preferences_message = (f"Eccoti un riassunto delle preferenze che hai indicato al bot."
+                           f" Se non ne hai indicato, ci saranno preferenze di base.\n\n")
+    preferences_message += f"<b>Numero di notizie per azione</b>: {logged_user['news_for_request']}\n"
+    if logged_user['localization'] is False:
+        preferences_message += "<b>Notizie localizzate in base al paese</b>: ❌\n"
+    else:
+        preferences_message += "<b>Notizie localizzate in base al paese</b>: ✅\n"
+    if len(logged_user['excluded_domains']) == 0:
+        preferences_message += "<b>Domini esclusi</b>: Nessuno\n"
+    else:
+        excluded_domains = f"<b>Domini esclusi</b>: {logged_user['excluded_domains'][0]}"
+        for d in logged_user['excluded_domains']:
+            excluded_domains += f", {d}"
+
+        preferences_message += f"{excluded_domains}\n"
+    bot.send_message(message.chat.id, text=preferences_message, parse_mode="HTML",
+                     reply_markup=preferences_markup(logged_user))
 
 
 if __name__ == '__main__':
